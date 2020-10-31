01a7ae3837031ed7537e99993e09dcfd2230b419
Tancredi Orlando <tancredi.orlando@gmail.com>
2020-10-31 18:49:11 +0100

    Add Separated combinator

---
+++ src/main/java/jmod/parser/combinator/text/Separated.java
@@ -0,0 +1,52 @@
+package jmod.parser.combinator.text;
+
+import jmod.parser.Parser;
+import jmod.parser.State;
+
+import static jmod.util.Lists.last;
+import static jmod.util.Lists.append;
+
+import java.util.List;
+
+public class Separated<ResultT> implements Parser<String, List<ResultT>> {
+    private Parser<String, ResultT> parser;
+    private String separator;
+    private Token separatorParser;
+
+    public Separated(Parser<String, ResultT> parser, String separator) {
+        this.parser = parser;
+        this.separator = separator;
+        this.separatorParser = new Token(separator);
+    }
+
+    @Override
+    public State<String, List<ResultT>>
+    parse(State.Success<String, List<ResultT>> s) {
+        State.Success<String, ResultT> state =
+            new State.Success<>(s.input,
+                                s.result == null || s.result.isEmpty()
+                                    ? null
+                                : last(s.result));
+
+        return parser.parse(state)
+            .match(elSuccess -> separatorParser.parse(
+                   new State.Success<String, String>(elSuccess.input, null))
+                   .match(sepSuccess -> this.parse(
+                              new State.Success<String, List<ResultT>>(
+                                  sepSuccess.input,
+                                  elSuccess.result == null
+                                      ? s.result == null
+                                          ? List.of()
+                                          : s.result
+                                      : append(s.result == null
+                                                   ? List.of()
+                                                   : s.result,
+                                               elSuccess.result))),
+                          sepFailure -> new State.Success<String, List<ResultT>>(
+                                            elSuccess.input,
+                                            append(s.result, elSuccess.result))),
+                   elFailure -> new State.Failure<String, List<ResultT>>(
+                                    elFailure.error));
+
+    }
+}
