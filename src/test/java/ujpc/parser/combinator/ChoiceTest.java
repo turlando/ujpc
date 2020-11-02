5004631615e370abc3d55f51e4fc19ce5cd1da49
Tancredi Orlando <tancredi.orlando@gmail.com>
2020-11-02 20:20:19 +0100

    Simplify Choice tests

--- src/test/java/ujpc/parser/combinator/ChoiceTest.java
+++ src/test/java/ujpc/parser/combinator/ChoiceTest.java
@@ -3,61 +3,34 @@ package ujpc.parser.combinator;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import static ujpc.parser.ParserTesting.canParse;
+import static ujpc.parser.ParserTesting.cantParse;
+
 import ujpc.parser.State;
 import ujpc.parser.combinator.text.Token;
 
 public class ChoiceTest {
-    @Test void parseFirst() {
-        Choice<String, String> parser
+    public static class TokenChoiceTest {
+        private final static Choice<String, String> PARSER
             = new Choice<>(
                 new Token("foo"),
                 new Token("bar"),
                 new Token("baz"));
-        State.Success<String, String> initial
-            = new State.Success<>("foo test", "");
-        State<String, String> result = parser.parse(initial);
-        State.Success<String, String> expected
-            = new State.Success<>(" test", "foo");
-        assertEquals(expected, result);
-    }
 
-    @Test void parseSecond() {
-        Choice<String, String> parser
-            = new Choice<>(
-                new Token("foo"),
-                new Token("bar"),
-                new Token("baz"));
-        State.Success<String, String> initial
-            = new State.Success<>("bar test", "");
-        State<String, String> result = parser.parse(initial);
-        State.Success<String, String> expected
-            = new State.Success<>(" test", "bar");
-        assertEquals(expected, result);
-    }
+        @Test void parseFirst() {
+            canParse(PARSER, "foo test", "foo", " test");
+        }
 
-    @Test void parseThird() {
-        Choice<String, String> parser
-            = new Choice<>(
-                new Token("foo"),
-                new Token("bar"),
-                new Token("baz"));
-        State.Success<String, String> initial
-            = new State.Success<>("baz test", "");
-        State<String, String> result = parser.parse(initial);
-        State.Success<String, String> expected
-            = new State.Success<>(" test", "baz");
-        assertEquals(expected, result);
-    }
+        @Test void parseSecond() {
+            canParse(PARSER, "bar test", "bar", " test");
+        }
 
-    @Test void parseWithFailure() {
-        Choice<String, String> parser
-            = new Choice<>(
-                new Token("foo"),
-                new Token("bar"),
-                new Token("baz"));
-        State.Success<String, String> initial
-            = new State.Success<>("quux test", "");
-        State<String, String> result = parser.parse(initial);
-        assertEquals(State.Failure.class, result.getClass());
+        @Test void parseThird() {
+            canParse(PARSER, "baz test", "baz", " test");
+        }
+
+        @Test void failure() {
+            cantParse(PARSER, "quux test");
+        }
     }
 }
