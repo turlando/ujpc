d95a9c88e592ec6d373b923171142f7926e598f9
Tancredi Orlando <tancredi.orlando@gmail.com>
2020-11-02 21:38:20 +0100

    Simplify MOD test

--- src/test/java/ujpc/example/mod/ModArchive_181474.java
+++ src/test/java/ujpc/example/mod/ModArchive_181474.java
@@ -1,14 +1,13 @@
 package ujpc.example.mod;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.fail;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
 
-import ujpc.parser.State;
-
 public class ModArchive_181474 {
-    private final ModParser PARSER = new ModParser();
+    private final static ModParser PARSER = new ModParser();
 
     private final static String FILE_NAME
         = "/ujpc/example/mod/" +
@@ -50,9 +49,9 @@ public class ModArchive_181474 {
         new Mod.Sample("",                           0,  0, 64, 0,    1),
         new Mod.Sample("",                           0,  0, 64, 0,    1));
 
-    private final int MOD_PATTERNS_COUNT = 37;
+    private final static int MOD_PATTERNS_COUNT = 37;
 
-    private final List<Integer> MOD_PATTERNS_TABLE = List.of(
+    private final static List<Integer> MOD_PATTERNS_TABLE = List.of(
          0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16,
         17, 18, 19,  8,  9, 20, 21,  6,  7, 26, 24, 22, 23, 27, 28, 29, 32,
         33, 30, 31,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
@@ -65,16 +64,14 @@ public class ModArchive_181474 {
     private final static String MOD_TYPE = "M.K.";
 
     private final byte[] content;
-    private final State.Success<byte[], Mod> initial;
     private final Mod result;
 
     public ModArchive_181474() throws java.io.IOException {
-        this.content = this.getClass().getResourceAsStream(FILE_NAME)
-                                      .readAllBytes();
-        this.initial = new State.Success<byte[], Mod>(content, null);
-        this.result = PARSER.parse(initial)
+        this.content = getClass().getResourceAsStream(FILE_NAME)
+                                 .readAllBytes();
+        this.result = PARSER.parse(content)
                              .match(s -> s.result,
-                                    f -> {throw new RuntimeException(f.error);});
+                                    f -> fail(f.error));
     }
 
     @Test void parseTitle() {
