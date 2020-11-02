d9853faa71b8d13bab7877ed5bd5383b7a58853e
Tancredi Orlando <tancredi.orlando@gmail.com>
2020-11-02 20:31:17 +0100

    Simplify Token tests

--- src/test/java/ujpc/parser/combinator/text/TokenTest.java
+++ src/test/java/ujpc/parser/combinator/text/TokenTest.java
@@ -3,36 +3,20 @@ package ujpc.parser.combinator.text;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import static ujpc.parser.ParserTesting.canParse;
+import static ujpc.parser.ParserTesting.cantParse;
+
 import ujpc.parser.Parser;
 import ujpc.parser.State;
 
 class TokenTest {
-    @Test void parseWithSuccess() {
-        Token parser = new Token("test");
-        State.Success<String, String> initial
-            = new State.Success<>("test", "");
-        State.Success<String, String> expected
-            = new State.Success<>("", "test");
-        State<String, String> result = parser.parse(initial);
-        assertEquals(expected, result);
+    private final static Token PARSER = new Token("test");
+
+    @Test void success() {
+        canParse(PARSER, "test", "test", "");
     }
 
-    @Test void parseWithFailure() {
-        Token parser = new Token("test");
-        State.Success<String, String> initial
-            = new State.Success<>("nope", "");
-        State<String, String> result = parser.parse(initial);
-        assertEquals(result.getClass(), State.Failure.class);
-    }
-
-    @Test void parseWithTransformation() {
-        Parser<String, Integer> parser
-            = new Token("42").map(Integer::parseInt);
-        State.Success<String, Integer> initial
-            = new State.Success<>("42", 0);
-        State.Success<String, Integer> expected
-            = new State.Success<>("", 42);
-        State<String, Integer> result = parser.parse(initial);
-        assertEquals(expected, result);
+    @Test void failure() {
+        cantParse(PARSER, "nope");
     }
 }
