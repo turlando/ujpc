975525abae2f53df9206769990679606cd37bbe1
Tancredi Orlando <tancredi.orlando@gmail.com>
2020-11-02 21:34:09 +0100

    Simplify JSON parser tests

--- src/test/java/ujpc/example/json/JsonParserTest.java
+++ src/test/java/ujpc/example/json/JsonParserTest.java
@@ -3,255 +3,147 @@ package ujpc.example.json;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import static ujpc.parser.ParserTesting.canParse;
+import static ujpc.parser.ParserTesting.cantParse;
+
 import ujpc.parser.State;
 
 import java.util.List;
 import java.util.Map;
 
 class JsonParserTest {
-    @Test void parseNull() {
-        JsonParser parser = new JsonParser();
-        State.Success<String, Json> initial
-            = new State.Success<>("null", new Json.Object());
-        State<String, Json> result = parser.parse(initial);
-        State.Success<String, Json> expected
-            = new State.Success<>("", new Json.Null());
-        assertEquals(expected, result);
+    private final static JsonParser PARSER = new JsonParser();
+
+    public class NullTest {
+        private final Json.Null n = new Json.Null();
+
+        @Test void simple() {
+            canParse(PARSER, "null", n, "");
+        }
+
+        @Test void spaces() {
+            canParse(PARSER, "  null ", n, " ");
+        }
     }
 
-    @Test void parseNullWithSpaces() {
-        JsonParser parser = new JsonParser();
-        State.Success<String, Json> initial
-            = new State.Success<>("  null ", new Json.Object());
-        State<String, Json> result = parser.parse(initial);
-        State.Success<String, Json> expected
-            = new State.Success<>(" ", new Json.Null());
-        assertEquals(expected, result);
+    public class BooleanTest {
+        private final Json.Boolean t = new Json.Boolean(true);
+        private final Json.Boolean f = new Json.Boolean(false);
+
+        @Test void simpleTrue() {
+            canParse(PARSER, "true", t, "");
+        }
+
+        @Test void spacesTrue() {
+            canParse(PARSER, "  true ", t, " ");
+        }
+
+        @Test void simpleFalse() {
+            canParse(PARSER, "false", f, "");
+        }
+
+        @Test void spacesFalse() {
+            canParse(PARSER, "  false ", f, " ");
+        }
     }
 
-    @Test void parseTrue() {
-        JsonParser parser = new JsonParser();
-        State.Success<String, Json> initial
-            = new State.Success<>("true", new Json.Object());
-        State<String, Json> result = parser.parse(initial);
-        State.Success<String, Json> expected
-            = new State.Success<>("", new Json.Boolean(true));
-        assertEquals(expected, result);
+    public class NumberTest {
+        @Test void simple() {
+            canParse(PARSER, "-42.23e7", new Json.Number(-42.23e7), "");
+        }
+
+        @Test void spaces() {
+            canParse(PARSER, "  -42.23e7 ", new Json.Number(-42.23e7), " ");
+        }
     }
 
-    @Test void parseTrueWithSpaces() {
-        JsonParser parser = new JsonParser();
-        State.Success<String, Json> initial
-            = new State.Success<>("  true ", new Json.Object());
-        State<String, Json> result = parser.parse(initial);
-        State.Success<String, Json> expected
-            = new State.Success<>(" ", new Json.Boolean(true));
-        assertEquals(expected, result);
+    public class StringTest {
+        @Test void emptyString() {
+            canParse(PARSER, "\"\"", new Json.String(""), "");
+        }
+
+        @Test void simple() {
+            canParse(PARSER, "\"foo bar baz\"",
+                     new Json.String("foo bar baz"), "");
+        }
+
+        @Test void spacesString() {
+            canParse(PARSER, "  \"foo bar baz\" ",
+                     new Json.String("foo bar baz"), " ");
+        }
+
+        @Test void numericString() {
+            canParse(PARSER, "\"42\"", new Json.String("42"), "");
+        }
     }
 
-    @Test void parseFalse() {
-        JsonParser parser = new JsonParser();
-        State.Success<String, Json> initial
-            = new State.Success<>("false", new Json.Object());
-        State<String, Json> result = parser.parse(initial);
-        State.Success<String, Json> expected
-            = new State.Success<>("", new Json.Boolean(false));
-        assertEquals(expected, result);
-    }
+    public class ArrayTest {
+        @Test void empty() {
+            canParse(PARSER, "[]", new Json.Array(List.of()), "");
+        }
 
-    @Test void parseFalseWithSpaces() {
-        JsonParser parser = new JsonParser();
-        State.Success<String, Json> initial
-            = new State.Success<>("false", new Json.Object());
-        State<String, Json> result = parser.parse(initial);
-        State.Success<String, Json> expected
-            = new State.Success<>("", new Json.Boolean(false));
-        assertEquals(expected, result);
-    }
+        @Test void singleNull() {
+            canParse(PARSER, "[null]",
+                     new Json.Array(new Json.Null()), "");
+        }
 
-    @Test void parseNumber() {
-        JsonParser parser = new JsonParser();
-        State.Success<String, Json> initial
-            = new State.Success<>("-42.23e7", new Json.Object());
-        State<String, Json> result = parser.parse(initial);
-        State.Success<String, Json> expected
-            = new State.Success<>("", new Json.Number(-42.23e7));
-        assertEquals(expected, result);
-    }
+        @Test void singleString() {
+            canParse(PARSER, "[\"foo\"]",
+                     new Json.Array(new Json.String("foo")), "");
+        }
 
-    @Test void parseNumberWithSpaces() {
-        JsonParser parser = new JsonParser();
-        State.Success<String, Json> initial
-            = new State.Success<>("  -42.23e7 ", new Json.Object());
-        State<String, Json> result = parser.parse(initial);
-        State.Success<String, Json> expected
-            = new State.Success<>(" ", new Json.Number(-42.23e7));
-        assertEquals(expected, result);
-    }
+        @Test void strings() {
+            canParse(PARSER, "[\"foo\",\"bar\"]",
+                     new Json.Array(new Json.String("foo"),
+                                    new Json.String("bar")),
+                     "");
+        }
 
-    @Test void parseEmptyString() {
-        JsonParser parser = new JsonParser();
-        State.Success<String, Json> initial
-            = new State.Success<>("\"\"", new Json.Object());
-        State<String, Json> result = parser.parse(initial);
-        State.Success<String, Json> expected
-            = new State.Success<>(
-                 "", new Json.String(""));
-        assertEquals(expected, result);
-    }
-
-    @Test void parseString() {
-        JsonParser parser = new JsonParser();
-        State.Success<String, Json> initial
-            = new State.Success<>("\"foo bar baz\"", new Json.Object());
-        State<String, Json> result = parser.parse(initial);
-        State.Success<String, Json> expected
-            = new State.Success<>(
-                "", new Json.String("foo bar baz"));
-        assertEquals(expected, result);
-    }
-
-    @Test void parseStringWithSpaces() {
-        JsonParser parser = new JsonParser();
-        State.Success<String, Json> initial
-            = new State.Success<>("  \"foo bar baz\" ", new Json.Object());
-        State<String, Json> result = parser.parse(initial);
-        State.Success<String, Json> expected
-            = new State.Success<>(
-                " ", new Json.String("foo bar baz"));
-        assertEquals(expected, result);
-    }
-
-    @Test void parseNumericString() {
-        JsonParser parser = new JsonParser();
-        State.Success<String, Json> initial
-            = new State.Success<>("\"42\"", new Json.Object());
-        State<String, Json> result = parser.parse(initial);
-        State.Success<String, Json> expected
-            = new State.Success<>(
-                "", new Json.String("42"));
-        assertEquals(expected, result);
-    }
-
-    @Test void parseEmptyArray() {
-        JsonParser parser = new JsonParser();
-        State.Success<String, Json> initial
-            = new State.Success<>("[]", new Json.Object());
-        State<String, Json> result = parser.parse(initial);
-        State.Success<String, Json> expected
-            = new State.Success<>(
-                "", new Json.Array(List.of()));
-        assertEquals(expected, result);
-    }
-
-    @Test void parseArrayOfNull() {
-        JsonParser parser = new JsonParser();
-        State.Success<String, Json> initial
-            = new State.Success<>("[null]", new Json.Object());
-        State<String, Json> result = parser.parse(initial);
-        State.Success<String, Json> expected
-            = new State.Success<>(
-                "", new Json.Array(new Json.Null()));
-        assertEquals(expected, result);
-    }
-
-    @Test void parseArrayOfString() {
-        JsonParser parser = new JsonParser();
-        State.Success<String, Json> initial
-            = new State.Success<>("[\"foo\"]", new Json.Object());
-        State<String, Json> result = parser.parse(initial);
-        State.Success<String, Json> expected
-            = new State.Success<>(
-                "", new Json.Array(new Json.String("foo")));
-        assertEquals(expected, result);
-    }
-
-    @Test void parseArrayOfStrings() {
-        JsonParser parser = new JsonParser();
-        State.Success<String, Json> initial
-            = new State.Success<>("[\"foo\",\"bar\"]", new Json.Object());
-        State<String, Json> result = parser.parse(initial);
-        State.Success<String, Json> expected
-            = new State.Success<>(
-                "", new Json.Array(new Json.String("foo"),
-                                   new Json.String("bar")));
-        assertEquals(expected, result);
-    }
-
-    @Test void parseNestedArray() {
-        JsonParser parser = new JsonParser();
-        State.Success<String, Json> initial
-            = new State.Success<>("[\"foo\",42,[\"foo\",42,[\"foo\",42]]]",
-                                  new Json.Object());
-        State<String, Json> result = parser.parse(initial);
-        State.Success<String, Json> expected
-            = new State.Success<>(
-                "",
-                new Json.Array(
-                    new Json.String("foo"),
-                    new Json.Number(42),
-                    new Json.Array(
+        @Test void nested() {
+            canParse(PARSER, "[\"foo\",42,[\"foo\",42,[\"foo\",42]]]",
+                     new Json.Array(
                         new Json.String("foo"),
                         new Json.Number(42),
                         new Json.Array(
                             new Json.String("foo"),
-                            new Json.Number(42)))));
-        assertEquals(expected, result);
+                            new Json.Number(42),
+                            new Json.Array(
+                                new Json.String("foo"),
+                                new Json.Number(42)))),
+                     "");
+        }
+
+        @Test void spaces() {
+            canParse(PARSER,
+                     "  [  \"foo\"  ,  \"bar\"  ] ",
+                     new Json.Array(new Json.String("foo"),
+                                    new Json.String("bar")),
+                     "");
+        }
     }
 
-    @Test void parseArrayWithSpaces() {
-        JsonParser parser = new JsonParser();
-        State.Success<String, Json> initial
-            = new State.Success<>(
-                "  [  \"foo\"  ,  \"bar\"  ] ",
-                new Json.Object());
-        State<String, Json> result = parser.parse(initial);
-        State.Success<String, Json> expected
-            = new State.Success<>(
-                " ", new Json.Array(new Json.String("foo"),
-                                    new Json.String("bar")));
-        assertEquals(expected, result);
-    }
+    public class ObjectTest {
+        @Test void empty() {
+            canParse(PARSER, "{}", new Json.Object(), "");
+        }
 
-    @Test void parseEmptyObject() {
-        JsonParser parser = new JsonParser();
-        State.Success<String, Json> initial
-            = new State.Success<>("{}", new Json.Object());
-        State<String, Json> result = parser.parse(initial);
-        State.Success<String, Json> expected
-            = new State.Success<>("", new Json.Object());
-        assertEquals(expected, result);
-    }
+        @Test void simple() {
+            canParse(PARSER, "{\"key1\": \"val1\"}",
+                     new Json.Object(Map.of(
+                         new Json.String("key1"), new Json.String("val1"))),
+                     "");
+        }
 
-    @Test void parseSimpleObject() {
-        JsonParser parser = new JsonParser();
-        State.Success<String, Json> initial
-            = new State.Success<>(
-                "{\"key1\": \"val1\"}",
-                new Json.Object());
-        State<String, Json> result = parser.parse(initial);
-        State.Success<String, Json> expected
-            = new State.Success<>("",
-                 new Json.Object(Map.of(
-                     new Json.String("key1"), new Json.String("val1"))));
-        assertEquals(expected, result);
-    }
-
-    @Test void parseObject() {
-        JsonParser parser = new JsonParser();
-        State.Success<String, Json> initial
-            = new State.Success<>(
-                "{\"key1\": \"val1\", \"key2\": 42, \"key3\": [23, \"42\"]}",
-                new Json.Object());
-        State<String, Json> result = parser.parse(initial);
-        State.Success<String, Json> expected
-            = new State.Success<>("",
-                new Json.Object(Map.of(
-                    new Json.String("key1"), new Json.String("val1"),
-                    new Json.String("key2"), new Json.Number(42),
-                    new Json.String("key3"), new Json.Array(
-                        new Json.Number(23),
-                        new Json.String("42")))));
-        assertEquals(expected, result);
+        @Test void heterogeneous() {
+            canParse(PARSER,
+                     "{\"key1\": \"val1\", \"key2\": 42, \"key3\": [23, \"42\"]}",
+                     new Json.Object(Map.of(
+                         new Json.String("key1"), new Json.String("val1"),
+                         new Json.String("key2"), new Json.Number(42),
+                         new Json.String("key3"), new Json.Array(
+                             new Json.Number(23),
+                             new Json.String("42")))),
+                     "");
+        }
     }
 }
