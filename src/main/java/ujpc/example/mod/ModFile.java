89f7b984f7bb9522dd4e6c630ca8313673af0ea7
Tancredi Orlando <tancredi.orlando@gmail.com>
2020-11-02 22:13:09 +0100

    Make ModFile useful

--- src/main/java/ujpc/example/mod/ModFile.java
+++ src/main/java/ujpc/example/mod/ModFile.java
@@ -5,12 +5,16 @@ import java.nio.file.Paths;
 import java.io.IOException;
 
 public class ModFile {
-    public final String  path;
-    public final byte[] content;
+    public final static ModParser PARSER = new ModParser();
+
+    public final String path;
+    public final byte[] bytes;
+    public final Mod    mod;
 
     public ModFile(String path) throws IOException {
-        this.path = path;
-        this.content = Files.readAllBytes(Paths.get(path));
+        this.path  = path;
+        this.bytes = Files.readAllBytes(Paths.get(path));
+        this.mod   = PARSER.parse(bytes).match(s -> s.result, f -> null);
     }
 
     @Override
