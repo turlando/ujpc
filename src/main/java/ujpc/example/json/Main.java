135fa1388adf35635c6cb075f231748f0a766460
Tancredi Orlando <tancredi.orlando@gmail.com>
2020-11-06 16:58:40 +0100

    Add small usage help in examples

--- src/main/java/ujpc/example/json/Main.java
+++ src/main/java/ujpc/example/json/Main.java
@@ -1,9 +1,26 @@
 package ujpc.example.json;
 
 public class Main {
+    private static final String COMMAND
+        = System.getProperty("sun.java.command");
+
+    private static final String USAGE
+        = String.format("Usage: java %s INPUT", COMMAND);
+
+    private static final String EXAMPLE
+        = String.format("Example: java %s '{\"key\": \"value\"}'", COMMAND);
+
+    private static final JsonParser PARSER = new JsonParser();
+
     public static void main(String[] args) {
-        final JsonParser parser = new JsonParser();
-        final Json result = parser.parse(args[0]).getOrThrow();
+        if (args.length != 1) {
+            System.err.println(USAGE);
+            System.err.println(EXAMPLE);
+            System.exit(1);
+        }
+
+        final Json result = PARSER.parse(args[0]).getOrThrow();
+
         System.out.println(result);
     }
 }
--- src/main/java/ujpc/example/mod/Main.java
+++ src/main/java/ujpc/example/mod/Main.java
@@ -1,20 +1,43 @@
 package ujpc.example.mod;
 
 public class Main {
+    private static final String COMMAND
+        = System.getProperty("sun.java.command");
+
+    private static final String USAGE
+        = String.format("Usage: java %s PATH", COMMAND);
+
+    private static final String EXAMPLE
+        = String.format("Example: java %s banger.mod", COMMAND);
+
+    private static void printInfo(Mod mod) {
+        System.out.println("Title: " + mod.title);
+        System.out.println("Samples:");
+
+        for (Mod.Sample sample : mod.samples)
+            System.out.println("  - " + sample);
+
+        System.out.println("Patterns: " + mod.patternsCount);
+        System.out.println("Type: " + mod.type);
+    }
+
     public static void main(String[] args) {
+        if (args.length != 1) {
+            System.err.println(USAGE);
+            System.err.println(EXAMPLE);
+            System.exit(1);
+        }
+
+        String path = args[0];
         ModFile file = null;
 
         try {
-            file = new ModFile(args[0]);
+            file = new ModFile(path);
         } catch (java.io.IOException e) {
             System.err.println(e);
             System.exit(1);
         }
 
-        System.out.println("Title: " + file.mod.title);
-        System.out.println("Samples:");
-
-        for (Mod.Sample sample : file.mod.samples)
-            System.out.println("  - " + sample);
+        printInfo(file.mod);
     }
 }
