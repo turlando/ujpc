aa4f99e8b92f6f1e90095d7935d5c62a3d5ebfa3
Tancredi Orlando <tancredi.orlando@gmail.com>
2020-11-07 18:16:57 +0100

    Reorder Mod parser

--- src/main/java/ujpc/example/mod/ModParser.java
+++ src/main/java/ujpc/example/mod/ModParser.java
@@ -20,6 +20,23 @@ public class ModParser implements Parser<byte[], Mod> {
     private final static int NOTES_COUNT          =   4;
     private final static int PATTERN_ROWS_COUNT   =  64;
 
+    private final Parser<byte[], Mod> parser
+        = new Ascii(TITLE_LENGTH).bind(title ->
+          new Repeat<byte[], Mod.Sample>(
+              SAMPLES_COUNT, new SampleParser()).bind(samples ->
+          new UInt8().bind(length ->
+          new UInt8().bind(restartPosition ->
+          new Repeat<byte[], Integer>(
+              PATTERN_TABLE_LENGTH, new UInt8()).bind(patternsTable ->
+          new Ascii(TYPE_LENGTH).bind(type ->
+          new PatternsParser(patternsTable).map(patterns ->
+              new Mod(title, samples, length, patternsTable,
+                      type, patterns))))))));
+
+    @Override
+    public State<byte[], Mod> parse(byte[] in)
+        { return parser.parse(in); }
+
     private static class SampleParser implements Parser<byte[], Mod.Sample> {
         private final static int NAME_LENGTH = 22;
 
@@ -34,9 +51,44 @@ public class ModParser implements Parser<byte[], Mod> {
                                  repeatOffset, repeatLength)))))));
 
         @Override
-        public State<byte[], Mod.Sample> parse(byte[] in) {
-            return parser.parse(in);
+        public State<byte[], Mod.Sample> parse(byte[] in)
+            { return parser.parse(in); }
+    }
+
+    private static class PatternsParser
+                   implements Parser<byte[], List<Mod.Pattern>> {
+        private final Parser<byte[], List<Mod.Pattern>> parser;
+
+        public PatternsParser(List<Integer> patternsTable) {
+            this.parser = new Repeat<byte[], Mod.Pattern>(
+                Collections.max(patternsTable),
+                new PatternParser());
         }
+
+        @Override
+        public State<byte[], List<Mod.Pattern>> parse(byte[] in)
+            { return parser.parse(in); }
+    }
+
+    private static class PatternParser implements Parser<byte[], Mod.Pattern> {
+        private final Parser<byte[], Mod.Pattern> parser
+            = new Repeat<byte[], Mod.Pattern.Row>(
+                PATTERN_ROWS_COUNT,
+                new RowParser()).map(x -> new Mod.Pattern(x));
+
+        @Override
+        public State<byte[], Mod.Pattern> parse(byte[] in)
+            { return parser.parse(in); }
+    }
+
+    private static class RowParser implements Parser<byte[], Mod.Pattern.Row> {
+        private final Parser<byte[], Mod.Pattern.Row> parser
+            = new Repeat<byte[], Mod.Pattern.Row.Note>(
+                NOTES_COUNT, new NoteParser()).map(x -> new Mod.Pattern.Row(x));
+
+        @Override
+        public State<byte[], Mod.Pattern.Row> parse(byte[] in)
+            { return parser.parse(in); }
     }
 
     private static class NoteParser
@@ -49,65 +101,7 @@ public class ModParser implements Parser<byte[], Mod> {
                     x & 0x0000000F));
 
         @Override
-        public State<byte[], Mod.Pattern.Row.Note> parse(byte[] in) {
-            return parser.parse(in);
-        }
-    }
-
-    private static class RowParser implements Parser<byte[], Mod.Pattern.Row> {
-        private final Parser<byte[], Mod.Pattern.Row> parser
-            = new Repeat<byte[], Mod.Pattern.Row.Note>(
-                NOTES_COUNT, new NoteParser()).map(x -> new Mod.Pattern.Row(x));
-
-        @Override
-        public State<byte[], Mod.Pattern.Row> parse(byte[] in) {
-            return parser.parse(in);
-        }
-    }
-
-    private static class PatternParser implements Parser<byte[], Mod.Pattern> {
-        private final Parser<byte[], Mod.Pattern> parser
-            = new Repeat<byte[], Mod.Pattern.Row>(
-                PATTERN_ROWS_COUNT,
-                new RowParser()).map(x -> new Mod.Pattern(x));
-
-        @Override
-        public State<byte[], Mod.Pattern> parse(byte[] in) {
-            return parser.parse(in);
-        }
-    }
-
-    private static class PatternList implements Parser<byte[], List<Mod.Pattern>> {
-        private final int length;
-        private final Parser<byte[], List<Mod.Pattern>> parser;
-
-        public PatternList(List<Integer> patternsTable) {
-            this.length = Collections.max(patternsTable);
-            this.parser = new Repeat<byte[], Mod.Pattern>(
-                length, new PatternParser());
-        }
-
-        @Override
-        public State<byte[], List<Mod.Pattern>> parse(byte[] in) {
-            return parser.parse(in);
-        }
-    }
-
-    private final Parser<byte[], Mod> parser
-        = new Ascii(TITLE_LENGTH).bind(title ->
-          new Repeat<byte[], Mod.Sample>(
-              SAMPLES_COUNT, new SampleParser()).bind(samples ->
-          new UInt8().bind(length ->
-          new UInt8().bind(restartPosition ->
-          new Repeat<byte[], Integer>(
-              PATTERN_TABLE_LENGTH, new UInt8()).bind(patternsTable ->
-          new Ascii(TYPE_LENGTH).bind(type ->
-          new PatternList(patternsTable).map(patterns ->
-              new Mod(title, samples, length, patternsTable,
-                      type, patterns))))))));
-
-    @Override
-    public State<byte[], Mod> parse(byte[] in) {
-        return parser.parse(in);
+        public State<byte[], Mod.Pattern.Row.Note> parse(byte[] in)
+            { return parser.parse(in); }
     }
 }
