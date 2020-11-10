1f69e9e6e998b3cfc884214151a9e7f220212eb6
Tancredi Orlando <tancredi.orlando@gmail.com>
2020-11-10 08:35:00 +0100

    Refactor Alphanum combinator

---
+++ src/main/java/ujpc/parser/text/combinator/Alphanum.java
@@ -0,0 +1,30 @@
+package ujpc.parser.text.combinator;
+
+import java.util.regex.Pattern;
+import ujpc.parser.Parser;
+import ujpc.parser.State;
+import ujpc.parser.text.Text;
+import static ujpc.util.Lists.first;
+import static ujpc.util.Strings.take;
+import static ujpc.util.Strings.drop;
+
+public class Alphanum implements Parser<Text, String> {
+    private Parser<Text, String> parser
+        = new Regex("([a-zA-Z0-9]+)").map(x -> first(x));
+
+    @Override
+    public State<Text, String> parse(Text in) {
+        State<Text, String> result = parser.parse(in);
+
+        return result.match(
+            success -> success,
+            failure -> new State.Failure<>(
+                in,
+                "Expected alphanumeric string"));
+    }
+
+    @Override
+    public String toString() {
+        return "Alphanum()";
+    }
+}
