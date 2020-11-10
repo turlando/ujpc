965f87d83928a2c9f93626934feee65c20206a07
Tancredi Orlando <tancredi.orlando@gmail.com>
2020-11-10 08:27:20 +0100

    Fix Parser testing utils

--- src/test/java/ujpc/parser/ParserTesting.java
+++ src/test/java/ujpc/parser/ParserTesting.java
@@ -5,18 +5,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ParserTesting {
     private ParserTesting() {}
 
-    public static <InputT, ResultT> void canParse(
-        Parser<InputT, ResultT> parser, InputT input,
-        ResultT expected, InputT leftover)
-    {
-        final State.Success<InputT, ResultT> expectedState
-            = new State.Success<>(leftover, expected);
-        assertEquals(expectedState, parser.parse(input));
+    public static <InputV, InputT extends Input<InputV>, ResultT>
+    void canParse(Parser<InputT, ResultT> parser, InputT input,
+                  ResultT expectedResult, InputV expectedLeftover) {
+        final State<InputT, ResultT> state    = parser.parse(input);
+        final ResultT                result   = state.getOrThrow();
+        final InputV                 leftover = state.input().rest();
+
+        assertEquals(expectedResult, result);
+        assertEquals(expectedLeftover, leftover);
     }
 
-    public static <InputT, ResultT> void cantParse(
-        Parser<InputT, ResultT> parser, InputT input
-    ) {
+    public static <InputT extends Input<?>, ResultT>
+    void cantParse(Parser<InputT, ResultT> parser, InputT input) {
         assertEquals(State.Failure.class, parser.parse(input).getClass());
     }
 }
