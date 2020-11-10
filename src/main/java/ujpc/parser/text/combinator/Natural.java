b392a6b8fcef3f97b525610326859b15ce587eed
Tancredi Orlando <tancredi.orlando@gmail.com>
2020-11-10 09:19:20 +0100

    Refactor Natural combinator

---
+++ src/main/java/ujpc/parser/text/combinator/Natural.java
@@ -0,0 +1,27 @@
+package ujpc.parser.text.combinator;
+
+import ujpc.parser.Parser;
+import ujpc.parser.State;
+import ujpc.parser.text.Text;
+import static ujpc.util.Lists.first;
+
+import java.util.regex.Pattern;
+
+public class Natural implements Parser<Text, String> {
+    private Parser<Text, String> parser
+        = new Regex("([+-]?[0-9]+)").map(x -> first(x));
+
+    @Override
+    public State<Text, String> parse(Text in) {
+        State<Text, String> result = parser.parse(in);
+
+        return result.match(
+            success -> success,
+            failure -> new State.Failure<>(in, "Expected natural number"));
+    }
+
+    @Override
+    public String toString() {
+        return "Natural()";
+    }
+}
