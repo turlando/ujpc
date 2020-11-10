126c1dc8c0ae90c615f5240915e43ab34d8dfad2
Tancredi Orlando <tancredi.orlando@gmail.com>
2020-11-10 09:29:35 +0100

    Refactor WhitespaceConsumer combinator

---
+++ src/main/java/ujpc/parser/text/combinator/WhitespaceConsumer.java
@@ -0,0 +1,24 @@
+package ujpc.parser.text.combinator;
+
+import ujpc.parser.Parser;
+import ujpc.parser.State;
+import ujpc.parser.text.Text;
+import ujpc.parser.combinator.Sequence;
+import static ujpc.util.Lists.first;
+
+public class WhitespaceConsumer<ResultT> implements Parser<Text, ResultT> {
+    private Parser<Text, ResultT> parser;
+
+    public WhitespaceConsumer(Parser<Text, ResultT> parser) {
+        this.parser = parser;
+    }
+
+    @Override
+    public State<Text, ResultT> parse(Text in) {
+        return new Sequence<Text, ResultT>(
+            new Whitespace().map(x -> null),
+            parser)
+        .map(x -> first(x))
+        .parse(in);
+    }
+}
