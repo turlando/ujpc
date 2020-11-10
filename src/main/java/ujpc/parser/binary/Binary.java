e4e7569bcbf3fd5f54d630c59a604ce71e2a860e
Tancredi Orlando <tancredi.orlando@gmail.com>
2020-11-10 10:57:10 +0100

    Fix Binary take and drop

--- src/main/java/ujpc/parser/binary/Binary.java
+++ src/main/java/ujpc/parser/binary/Binary.java
@@ -8,9 +8,12 @@ extends Input<byte[]> {
     public Binary(byte[] input)             { super(input, 0); }
     public Binary(byte[] input, int offset) { super(input, offset); }
 
-    @Override public byte[] rest()      { return Bytes.drop(input(), offset()); }
-    @Override public byte[] take(int n) { return Bytes.take(input(), offset(), n); }
-    @Override public byte[] drop(int n) { return Bytes.drop(input(), offset(), n); }
+    @Override public byte[] rest()
+        { return Bytes.drop(input(), offset()); }
+    @Override public byte[] take(int n)
+        { return Bytes.take(input(), offset(), offset() + n); }
+    @Override public byte[] drop(int n)
+        { return Bytes.drop(input(), offset(), offset() + n); }
 
     @Override public String position()  { return String.format("%X", offset()); }
     @Override public String needle()    { return "To be implemented."; }
